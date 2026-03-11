b8149c1bc08cb56ddba66f9007c42a6efd01e696
christianlewis21 <christianelewis64@gmail.com>
2026-03-11 16:30:02 -0400

    change keys

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 
 const url = process.env.SUPABASE_URL;
-const key = process.env.SUPABASE_ANON_KEY;
+const key = process.env.SUPABASE_KEY;
 const supabase = createClient(url, key)
 app.use(express.json());
 
