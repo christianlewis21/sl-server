8a97d00773e06497bb1314199209b9b68f67e4b9
christianlewis21 <christianelewis64@gmail.com>
2026-03-11 16:21:51 -0400

    create post for supabase

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+const url = process.env.SUPABASE_URL;
+const key = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(url, key)
+app.use(express.json());
+
+app.post('/submit', async (req, res) => {
+    const data = req.body;
+
+    try {
+        const { error } = await supabase
+        .from('stats_leaderboard')
+        .insert([data]);
+
+        if (error) {
+            console.error(`Supabase insert error: ${error}`)
+            return res.status(500).json({ status: 'error', error});
+        }
+
+        res.json({ status: 'ok'});
+    }   catch (err) {
+        console.error(`Unexpected error: ${err}`);
+        res.status(500).json({ status: 'error', error: err.message})
+    }
 });
 
 app.listen(port, '0.0.0.0', () => {
