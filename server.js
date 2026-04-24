849715cfca633b0f5b09efc47f5622514a74c6ba
christianlewis21 <christianelewis64@gmail.com>
2026-04-24 15:50:54 -0400

    Revert "cache leaderboard on upload for faster download times"
    This reverts commit 8c140dcb62392d46ba8b4acfa4e90105e79d9abc.
    same reasoning as last commit

--- server.js
+++ server.js
@@ -4,54 +4,51 @@ const port = 80;
 const { createClient } = require('@supabase/supabase-js')
 require('dotenv').config()
 
+
 const url = process.env.SUPABASE_URL;
 const key = process.env.SUPABASE_KEY;
 const supabase = createClient(url, key)
 app.use(express.json());
 
-let leaderboardCache = [];
-
-async function refreshCache() {
-  const { data, error } = await supabase
-    .from('stats_leaderboard')
-    .select('*')
-    .order('steam_id', { ascending: false });
-
-  if (error) {
-    console.error("Failed to refresh cache:", error);
-    return;
-  }
-
-  leaderboardCache = data;
-  console.log("Leaderboard cache refreshed");
-}
-
 app.post('/submit', async (req, res) => {
-  const data = req.body;
+    const data = req.body;
 
+    try {
+        const { error } = await supabase
+        .from('stats_leaderboard')
+        .upsert([data], { onConflict: "steam_id "});
+
+        if (error) {
+            console.error("Supabase insert error:", error);
+            return res.status(500).json({ status: 'error', error});
+        }
+
+        res.json({ status: 'ok'});
+    }   catch (err) {
+        console.error("Unexpected error:", err);
+        res.status(500).json({ status: 'error', error: err.message})
+    }
+});
+
+app.get('/leaderboard', async (req, res) => {
   try {
-    const { error } = await supabase
+    const { data, error } = await supabase
       .from('stats_leaderboard')
-      .upsert([data], { onConflict: "steam_id" });
+      .select('*')
+      .order('steam_id', { ascending: false })
 
     if (error) {
-      console.error("Supabase insert error:", error);
+      console.error("Supabase fetch error:", error);
       return res.status(500).json({ status: 'error', error });
     }
 
-    await refreshCache();
-    res.json({ status: 'ok' });
+    res.json(data);
   } catch (err) {
-    console.error("Unexpected error:", err);
+    console.error("Unexpected fetch error:", err);
     res.status(500).json({ status: 'error', error: err.message });
   }
 });
 
-app.get('/leaderboard', (req, res) => {
-  res.json(leaderboardCache);
-});
-
-app.listen(port, '0.0.0.0', async () => {
-  await refreshCache();
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
